1112c345366d7e7d87fe3aacf3053e9cff93d33d
Edna Rocio Aguilar <eraj8@yahoo.com>
2022-09-06 19:25:20 -0500

    https://github.com/EdnaRocioAguilar/Mascotas.git

---
+++ MascotaFeliz.App.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace MascotaFeliz.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
