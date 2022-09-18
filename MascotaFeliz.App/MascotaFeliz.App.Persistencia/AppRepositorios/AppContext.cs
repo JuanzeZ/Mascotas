aa816e0350c096cf973719f8516fa0e5f1ad333e
Jorge Leonardo <klichorf123@hotmail.com>
2022-09-18 17:35:28 -0500

    este es un ejemplo

---
+++ MascotaFeliz.App/MascotaFeliz.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,31 @@
+using MascotaFeliz.App.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace MascotaFeliz.App.Persistencia
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+
+        public DbSet<Veterinario> Veterinarios { get; set; }
+
+        public DbSet<Dueno> Duenos { get; set; }
+
+        public DbSet<VisitaPyP> VisitasPyP { get; set; }
+
+        public DbSet<Historia> Historias { get; set; }
+
+        public DbSet<Mascota> Mascotas { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder
+        )
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = MascotaFelizData");
+            }
+        }
+    }
+}
